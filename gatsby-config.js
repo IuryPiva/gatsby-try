fdf919bdbfd09ca975130ecaf2965a16b95722b8
Iury Piva <iurypiva@gmail.com>
2021-01-19 18:56:18 -0300

    update meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: 'Iury a JS dev',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
+        name: 'Iury a JS dev',
+        short_name: 'Iury a JS dev',
         start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        icon: 'src/images/js.jpeg',
       },
     },
   ],
